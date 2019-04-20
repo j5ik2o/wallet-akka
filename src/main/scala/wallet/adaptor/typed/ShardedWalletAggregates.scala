d9ade13dcac0a2fc473792503b106b958d35d68a
Junichi Kato <j5ik2o@gmail.com>
2019-04-20 21:46:06 +0900

    refactor

--- src/main/scala/wallet/adaptor/typed/ShardedWalletAggregates.scala
+++ src/main/scala/wallet/adaptor/typed/ShardedWalletAggregates.scala
@@ -1,9 +1,9 @@
 package wallet.adaptor.typed
 
-import akka.actor.typed.ActorRef
+import akka.actor.typed.{ ActorRef, Behavior }
 import akka.actor.typed.scaladsl.Behaviors
 import akka.cluster.sharding.typed.ShardingEnvelope
-import akka.cluster.sharding.typed.scaladsl.{ ClusterSharding, Entity, EntityTypeKey }
+import akka.cluster.sharding.typed.scaladsl.{ ClusterSharding, Entity, EntityContext, EntityTypeKey }
 import wallet.adaptor.typed.WalletProtocol.{ CommandRequest, Idle, Stop }
 
 import scala.concurrent.duration.FiniteDuration
@@ -11,6 +11,28 @@ import scala.concurrent.duration.FiniteDuration
 object ShardedWalletAggregates {
 
   val TypeKey: EntityTypeKey[CommandRequest] = EntityTypeKey[CommandRequest]("wallets")
+
+  private def behavior(requestLimit: Int, receiveTimeout: FiniteDuration): EntityContext => Behavior[CommandRequest] = {
+    entityContext =>
+      Behaviors.setup[CommandRequest] { ctx =>
+        val childRef = ctx.spawn(
+          WalletAggregates.behavior(requestLimit)(PersistentWalletAggregate.behavior),
+          name = WalletAggregates.name
+        )
+        ctx.watch(childRef)
+        ctx.setReceiveTimeout(receiveTimeout, Idle)
+        Behaviors.receiveMessage[CommandRequest] {
+          case Idle =>
+            entityContext.shard ! ClusterSharding.Passivate(ctx.self)
+            Behaviors.same
+          case Stop =>
+            Behaviors.stopped
+          case msg =>
+            childRef ! msg
+            Behaviors.same
+        }
+      }
+  }
 
   def initClusterSharding(
       clusterSharding: ClusterSharding,
@@ -21,27 +43,7 @@ object ShardedWalletAggregates {
       clusterSharding.init(
         Entity(
           typeKey = TypeKey,
-          createBehavior = { entityContext =>
-            Behaviors.setup[CommandRequest] {
-              ctx =>
-                val childRef = ctx.spawn(
-                  WalletAggregates.behavior(requestLimit)(PersistentWalletAggregate.behavior),
-                  name = WalletAggregates.name
-                )
-                ctx.watch(childRef)
-                ctx.setReceiveTimeout(receiveTimeout, Idle)
-                Behaviors.receiveMessage[CommandRequest] {
-                  case Idle =>
-                    entityContext.shard ! ClusterSharding.Passivate(ctx.self)
-                    Behaviors.same
-                  case Stop =>
-                    Behaviors.stopped
-                  case msg =>
-                    childRef ! msg
-                    Behaviors.same
-                }
-            }
-          }
+          createBehavior = behavior(requestLimit, receiveTimeout)
         ).withStopMessage(Stop)
       )
     shardRegion
